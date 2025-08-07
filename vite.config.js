036e63c4fc5f7786cf32653536ffa5843e1ecaa4
syam <syamrc46@gmail.com>
2025-08-07 08:59:19 +0530

    fix

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: './', // <-- this is important for Netlify if deploying to a subdirectory
+})
